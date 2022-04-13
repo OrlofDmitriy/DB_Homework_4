ddf3a549c60e261da1b9c750158650d352756258
Дмитрий Орлов <orlof@mail.ru>
2022-04-13 10:24:21 +0500

    Добавлены INSERT запросы для заполнения таблиц связей

--- insert.py
+++ insert.py
@@ -76,6 +76,72 @@ collections = [
     ('Old songs collection', 2018)
 ]
 
+artist_genre = [
+    (3, 1),
+    (3, 2),
+    (4, 3),
+    (4, 4),
+    (5, 7),
+    (5, 8),
+    (6, 5),
+    (6, 6),
+    (8, 9),
+    (8, 10)
+]
+
+artist_album = [
+    (1, 1),
+    (1, 2),
+    (2, 3),
+    (3, 4),
+    (3, 5),
+    (7, 6),
+    (7, 7),
+    (8, 8),
+    (8, 9),
+    (9, 10),
+    (10, 11)
+]
+
+track_collection = [
+    (1, 1),
+    (2, 1),
+    (3, 1),
+    (4, 1),
+    (5, 1),
+    (6, 1),
+    (7, 1),
+    (1, 2),
+    (2, 2),
+    (3, 2),
+    (4, 2),
+    (5, 2),
+    (8, 3),
+    (9, 3),
+    (10, 3),
+    (11, 3),
+    (13, 4),
+    (14, 4),
+    (15, 4),
+    (16, 4),
+    (8, 5),
+    (10, 5),
+    (13, 5),
+    (17, 5),
+    (20, 6),
+    (21, 6),
+    (22, 6),
+    (23, 6),
+    (1, 7),
+    (8, 7),
+    (13, 7),
+    (22, 7),
+    (1, 8),
+    (5, 8),
+    (20, 8),
+    (21, 8)
+]
+
 db = 'postgresql://orlof:12345@localhost:5432/homeworkdb'
 engine = sqlalchemy.create_engine(db)
 connection = engine.connect()
@@ -97,3 +163,15 @@ for track in tracks:
 for collection in collections:
     connection.execute(f"""INSERT INTO collection (name, release_year)
     VALUES ('{collection[0]}', {collection[1]});""")
+
+for element in artist_genre:
+    connection.execute(f"""INSERT INTO artistgenre (genre_id, artist_id)
+    VALUES ({element[0]}, {element[1]});""")
+
+for element in artist_album:
+    connection.execute(f"""INSERT INTO artistalbum (artist_id, album_id)
+    VALUES ({element[0]}, {element[1]});""")
+
+for element in track_collection:
+    connection.execute(f"""INSERT INTO trackcollection (track_id, collection_id)
+    VALUES ({element[0]}, {element[1]});""")
